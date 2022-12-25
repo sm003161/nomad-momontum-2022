9891f7c2ad3375991ede7438655e861367f2e59e
sm003161 <sm003161@naver.com>
2022-12-25 15:30:15 +0900

    Turn a form div into an actual form

---
+++ 20221225/app.js
@@ -0,0 +1,12 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
+const loginSubmit = loginForm.querySelector("button");
+
+function login() {
+    // 자주 쓰는 선택자 변수 만들기 ( 선택 )
+    const inputValue = loginInput.value;
+
+    // 사용자 이름 변수 만들기
+    let userName = "";
+
+}
