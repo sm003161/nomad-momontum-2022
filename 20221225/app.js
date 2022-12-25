edf766b184e137f3647d6a1e98ded7a74d9a44ab
sm003161 <sm003161@naver.com>
2022-12-25 17:06:52 +0900

    main

--- 20221225/app.js
+++ 20221225/app.js
@@ -1,14 +1,23 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
-const loginSubmit = loginForm.querySelector("button");
-const link = document.querySelector("a");
+const welcomeMessage = document.querySelector("#welcomeMessage");
 
 function login(event) {
     // 자동 Refresh 막기
     event.preventDefault();
 
     // input 값 console.log 하기
-    console.log(loginInput.value);
+    // console.log(loginInput.value);
+
+    // input 값 변수화
+    const userName = loginInput.value;
+
+    // input 숨기고 h1 표시하기
+    loginForm.classList.add("hidden");
+    welcomeMessage.className = "show";
+
+    // welcomeMessage 표시하기
+    welcomeMessage.innerText = `Hello, ${userName}!`;
 }
 
 loginForm.addEventListener("submit", login);
