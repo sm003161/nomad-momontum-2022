ffa87bc637b07ffcedc06ce0a6a499fb49fd61de
sm003161 <sm003161@naver.com>
2022-12-30 23:37:27 +0900

    Adding a local storage conditional

--- 20221225/app.js
+++ 20221225/app.js
@@ -2,22 +2,28 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = loginForm.querySelector("input");
 const welcomeMessage = document.querySelector("#welcomeMessage");
 
+const HIDDEN_CLASSNAME = "hidden";
+
+// 로그인 이벤트
 function login(event) {
     // 자동 Refresh 막기
     event.preventDefault();
 
-    // input 값 console.log 하기
-    // console.log(loginInput.value);
-
-    // input 값 변수화
+    // localStorage 에 저장하기
     const userName = loginInput.value;
+    localStorage.setItem( 'user', userName );
 
-    // input 숨기고 h1 표시하기
-    loginForm.classList.add("hidden");
-    welcomeMessage.className = "show";
-
-    // welcomeMessage 표시하기
-    welcomeMessage.innerText = `Hello, ${userName}!`;
+    // 완료되면 새로고침
+    window.location.reload();
 }
 
 loginForm.addEventListener("submit", login);
+
+// localStorage 에 userName 이 있으면 input 숨기고 h1 표시하기
+const savedUserName = localStorage.user;
+
+if( savedUserName != null ){
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    welcomeMessage.classList.remove(HIDDEN_CLASSNAME); 
+    welcomeMessage.innerText = `Hello, ${savedUserName}!`;
+}
