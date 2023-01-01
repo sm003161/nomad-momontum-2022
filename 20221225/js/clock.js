5c3c7c082d6e5eeeb2bf5e46592e4f3c7a420c2e
sm003161 <sm003161@naver.com>
2023-01-01 18:10:26 +0900

    Making a clock V1

--- 20221225/js/clock.js
+++ 20221225/js/clock.js
@@ -1,7 +1,31 @@
 const clock = document.querySelector("p#clock");
+const h = clock.querySelector("span#h");
+const m = clock.querySelector("span#m");
+const s = clock.querySelector("span#s");
 
+/*
 function hello() {
     console.log("hello");
 }
 
-setInterval(hello, 5000);
+// 5초마다 hello 실행하기
+// setInterval(hello, 5000);
+
+// 5초 후에 hello 실행하기
+// setTimeout(hello, 5000);
+
+// 오늘 날짜 console.log 하기
+console.log(new Date);
+*/
+
+function loadTime() {
+    hours = new Date().getHours();
+    minutes = new Date().getMinutes();
+    secounds = new Date().getSeconds();
+    h.innerHTML = `${hours}`;
+    m.innerHTML = `${minutes}`;
+    s.innerHTML = `${secounds}`;
+}
+
+loadTime();
+setInterval(loadTime, 1000);
